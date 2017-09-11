ceebd5618b8472eb159bb8f460cdc6d7e2f46d4b
libin <libin@3ti.us>
2017-09-11 15:48:09 +0800

    url

--- config/url.sl.php
+++ config/url.sl.php
@@ -5,10 +5,11 @@ return [
     //sl
     'sl/<controller>/<action>' => 'sl/<controller>/<action>',
 
-    'sl/schedule/edit-schedule/<sche_id:\d+>' => 'sl/schedule/update-schedule',
+    'sl/schedule/edit-schedule/<data_type:\w+>/<sche_id:\d+>' => 'sl/schedule/update-schedule',
+
+    'sl/schedule/add-schedule/<data_type:\w+>' => 'sl/schedule/add-schedule',
 
     'sl/schedule/task-sche-crontab/<sche_id:\d+>' => 'sl/schedule/task-sche-crontab',
 
     'sl/schedule/task-item/<cron_id:\d+>' => 'sl/schedule/task-item',
-
 ];
